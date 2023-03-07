668a380b0b3da26586a19e4c684b640b77c81f59
Maissoun Ksara <maissounksara@gmail.com>
2023-03-07 02:04:22 -0500

    fixed order view

--- static/js/drinkView.js
+++ static/js/drinkView.js
@@ -1,4 +1,12 @@
 function DrinkView(numDrinks) {
+    this.update = (data) => {
+        if (!Array.isArray(data)) {
+            console.error("Invalid data format, expected an array of drinks");
+            return;
+        }
+
+        this.updateDrinks(data);
+    }
 
     this.updateDrinks = (drinks) => {
         $('#drinktable').empty();
@@ -7,21 +15,23 @@ function DrinkView(numDrinks) {
             <div class="table-responsive">
                 <table class="table">
                     <thead>
-                    <tr>
-                        <th style="width: 40%">Name</th>
-                    </tr>
+                        <tr>
+                            <th style="width: 40%">Name</th>
+                        </tr>
                     </thead>
                     <tbody></tbody>
+                </table>
+            </div>
         `);
         $('#drinktable').append(columnLabels);
 
-        
-
-        for(let i = 0; i < numDrinks; i++){
+        for (let i = 0; i < numDrinks && i < drinks.length; i++) {
             const drink = drinks[i];
 
             const drinkRow = $(`
-                    <td class="tableItem" style="width: 40%">${Item.name}</td>
+                <tr>
+                    <td class="tableItem" style="width: 40%">${drink.name}</td>
+                </tr>
             `);
 
             $(drinkRow).find('.dec-button').on('click', _ => {
@@ -32,24 +42,16 @@ function DrinkView(numDrinks) {
             });
 
             $(table).append(drinkRow);
-
         }
-        $('#drinktable').append(table);
 
+        $('#drinktable').append(table);
         const closing = $(`</tbody></table></div></div>`);
         $('#drinktable').append(closing);
     }
 
-    this.update = (data) => {
-        this.updateDrinks(data.drinks);
-    }
-
     this.load = () => {
-        $.get('/api/getItems', {
-        }, (data) => {
+        $.get('/api/getItems', {}, (data) => {
             this.update(data);
         });
-    }
-  
+    };
 }
-
